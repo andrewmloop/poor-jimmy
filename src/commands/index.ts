38a39eec0470ab00ba5db520deb4a735628daad4
AML <andrewmloop@gmail.com>
2022-08-28 21:04:09 -0400

    Add now playing command shell

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,9 @@
+import { nowPlaying } from "./now-playing";
 import { pause } from "./pause";
 import { ping } from "./ping";
 import { play } from "./play";
 import { resume } from "./resume";
 
-const commands = [pause, ping, play, resume];
+const commands = [nowPlaying, pause, ping, play, resume];
 
 export default commands;
