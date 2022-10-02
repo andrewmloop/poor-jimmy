0f9270df314bd6bda949ac05aea9aeeb2f297c37
AML <andrewmloop@gmail.com>
2022-10-02 18:09:31 -0400

    Clean up currentQ command and reply logic

--- src/commands/currentQ.ts
+++ src/commands/currentQ.ts
@@ -14,18 +14,28 @@ export default class CurrentQ extends Command {
 
     const guildId = interaction.guildId as string;
     const activeQueue = this.client.activeQueueMap.get(guildId);
-    const currentQ = "Current queue:\n" + activeQueue?.name + "\n\n";
 
-    let trackList = "Tracks:\n";
-    const tracks = activeQueue?.tracks;
-    if (tracks && tracks.length > 0) {
-      tracks.forEach((track, index) => {
-        trackList = trackList + "#" + (index + 1) + ": " + track.title + "\n";
-      });
-    } else {
-      trackList = "No tracks in this queue";
+    if (!activeQueue) {
+      this.handleReply(interaction, "No active queue found!");
+      return;
     }
 
-    await interaction.editReply(currentQ + trackList);
+    let replyString = `Name: ${activeQueue.name}\n\nTracks:\n`;
+
+    const tracks = activeQueue.tracks;
+
+    if (!tracks || tracks.length === 0) {
+      replyString = replyString + `Nothing queued!`;
+      this.handleReply(interaction, replyString);
+    } else {
+      tracks.forEach((track, index) => {
+        replyString = replyString + this.formatListItem(track.title, index);
+      });
+      this.handleReply(interaction, replyString);
+    }
   };
+
+  private formatListItem(title: string, index: number): string {
+    return `#${index + 1}: ${title}\n`;
+  }
 }
