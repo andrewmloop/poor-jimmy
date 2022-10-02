20fabaec7bdccf4efe778c6fc1771e5fc74adcdd
AML <andrewmloop@gmail.com>
2022-10-02 15:33:45 -0400

    Clean up logic and responses

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -1,14 +1,4 @@
-import {
-  AudioPlayer,
-  AudioPlayerStatus,
-  createAudioResource,
-  entersState,
-  StreamType,
-} from "@discordjs/voice";
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
-import { Track } from "../utils/Bot";
-import { Command } from "../utils/Command";
-import ytdl from "ytdl-core";
 import { PlayCommand } from "../utils/PlayCommand";
 
 export default class Skip extends PlayCommand {
@@ -20,41 +10,36 @@ export default class Skip extends PlayCommand {
     .setDescription(this.description);
 
   execute = async (interaction: CommandInteraction): Promise<void> => {
+    interaction.deferReply();
+
     const guildId = interaction.guildId as string;
-    const activeQueueMap = this.client.activeQueueMap;
-    const serverQueue = this.client.activeQueueMap.get(guildId);
+    const activeQueue = this.client.activeQueueMap.get(guildId);
     const player = this.getAudioPlayer(guildId);
 
-    if (serverQueue != null) {
-      const tracks = serverQueue?.tracks;
-
-      if (!tracks || tracks.length === 0) {
-        interaction.reply("There is nothing in the queue!");
-        return;
-      }
-
-      // this.handleTrackFinish(guildId, activeQueueMap, serverQueue);
-
-      const current = tracks[0];
-      if (serverQueue.isLoop) {
-        tracks.push(current);
-      }
-      tracks.shift();
-      this.playTrack(tracks[0], player);
+    if (!activeQueue) {
+      this.handleReply(interaction, "No queue found!");
+      return;
     }
+
+    const tracks = activeQueue.tracks;
+
+    if (!tracks || tracks.length === 0) {
+      this.handleReply(interaction, "There is nothing to skip!");
+      return;
+    }
+
+    const current = tracks[0];
+
+    if (activeQueue.isLoop) {
+      tracks.push(current);
+    }
+    tracks.shift();
+    this.playTrack(tracks[0], player);
+
+    this.handleReply(interaction, "Track skipped!");
   };
 
-  // private async playTrack(track: Track, player: AudioPlayer): Promise<void> {
-  //   const stream = ytdl(track.url, {
-  //     filter: "audioonly",
-  //     highWaterMark: 1 << 25,
-  //   });
-  //   const resource = createAudioResource(stream, {
-  //     inputType: StreamType.Arbitrary,
-  //   });
-  //   player.stop();
-  //   await entersState(player, AudioPlayerStatus.Idle, 5_000);
-  //   player.play(resource);
-  //   await entersState(player, AudioPlayerStatus.Playing, 5_000);
-  // }
+  private handleReply(interaction: CommandInteraction, message: string): void {
+    interaction.editReply(message);
+  }
 }
